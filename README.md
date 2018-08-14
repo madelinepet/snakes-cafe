d3aeb32026f7855f34353ac3f2a756d8f12318de
MADELINE PETERS <madelinepet@hotmail.com>
2018-08-13 17:22:21 -0700

    Program runs, quits, prompts user, prompts user after each order, responds to unrecognized inputs, and responds to multiple orders of the same item, tells user order has been placed when done

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # snakes-cafe
 
+**Author**: Madeline Peters
+**Version**: 1.0.0
+
+## Overview
+### This project aims to provide an interactive experience for a user to order from a menu.
+
+## Getting Started
+### To get started, start up your virtual environment (type "pipenv shell" in your terminal), and then run teh file in your terminal using: "python3 snakes_cafe.py"
+
+## Architecture
+### Python 3, VS Code
+
+## API
+### N/A
+
+## Change Log
+### Monday 8/13/2018 3:30 PM: Scaffolding set up and first readme update completed.
--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,17 +1,88 @@
-**Author**: Madeline Peters
-**Version**: 1.0.0
+from textwrap import dedent
+import sys
 
-## Overview
-### This project aims to provide an interactive experience for a user to order from a menu.
+WIDTH = 96
 
-## Getting Started
-### To get started, start up your virtual environment (type "pipenv shell" in your terminal), and then run teh file in your terminal using: "python3 snakes_cafe.py"
+BANK = [
+    {
+        'question': 'What would you like to order?',
+        'number': 0,
+    },
+]
+appetizers = ['wings', 'cookies', 'spring rolls']
 
-## Architecture
-### Python 3, VS Code
+entrees = ['salmon', 'steak', 'meat tornado', 'a literal garden']
 
-## API
-### N/A
+desserts = ['ice cream', 'cake', 'pie']
 
-## Change Log
-### Monday 8/13/2018 3:30 PM: Scaffolding set up and first readme update completed.
+drinks = ['coffee', 'tea', 'blood of the innocent']
+
+order = []
+
+def greeting():
+    """Function that greets the user and shows the menu.
+    """
+    ln_one = 'Welcome to the Snakes Cafe!'
+    ln_two = 'Please see our menu below.'
+    ln_three = 'To quit at any time, type "quit"'
+    print(dedent(f'''
+            {'*' * WIDTH}
+            {' ' * ((WIDTH - len(ln_one) // 2)) + ln_one + (' ' * ((WIDTH - len(ln_one)) // 2))}
+            {' ' * ((WIDTH - len(ln_two) // 2)) + ln_two + (' ' * ((WIDTH - len(ln_two)) // 2))}
+
+            {' ' * ((WIDTH - len(ln_three) // 2)) + ln_three + (' ' * ((WIDTH - len(ln_three)) // 2))}
+            {'*' * WIDTH}
+        '''))
+    print('Appetizers')
+    print('-----------')
+    for i in appetizers:
+        print(i)
+
+    print(' \n')
+    print('Entrees')
+    print('----------')
+    for i in entrees:
+        print(i)
+
+    print(' \n')
+    print('Desserts')
+    print('----------')
+    for i in desserts:
+        print(i)
+
+    print(' \n')
+    print('Drinks')
+    print('----------')
+    for i in drinks:
+        print(i)
+    print(' \n')
+
+
+def check():
+    user_input = input('What would you like to order? ').lower()
+    if user_input == 'quit':
+        exit()
+    elif user_input in appetizers or user_input in entrees or user_input in desserts or user_input in drinks:
+        if user_input in order:
+            print('You must be hungry! Now you have multiple orders of ' + user_input  + '!')
+            order.append(user_input)
+        else:
+            print('One order of ' + user_input + ' has been added to your cart! ')
+            order.append(user_input)
+        check()
+    else:
+        print('Please look at our menu and choose something we have!')
+        check()
+
+def run():
+    greeting()
+    check()
+
+def exit():
+    print(dedent('''
+    Thank you! Your order has been placed!
+    '''))
+    sys.exit()
+
+if __name__ == '__main__':
+    run()
